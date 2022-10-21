34052973b815ff4fd3bc26c2e55457c012623c8d
Guilherme Chenci <guilherme.chenci@totvs.com.br>
2022-10-21 17:40:30 -0300

    Curso POUI

---
+++ po-transfer/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { PoDynamicFormField, PoMenuItem } from '@po-ui/ng-components';
+import { PoDynamicField } from '@po-ui/ng-components/lib/components/po-dynamic/po-dynamic-field.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Inicio', action: () => alert('Hello world') }
+  ];
+ 
+  propertyForm: Array<PoDynamicFormField> = [
+    { property: 'sender', label:'Remetente', placeholder:'Remetente', required:true },
+    { property: 'recipient', label:'Destinatário', placeholder:'Destinatário', required:true },
+    { property: 'money', label:'Valor', type:'currency', placeholder:'Valor', required:true },
+    { property: 'description', label:'Descrição', required:true }
+   ]
+
+}
